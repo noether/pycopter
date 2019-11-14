3d3a43f88e4dabae578909244961436a8d4dbf7d
Hector Garcia de Marina <hgdemarina@gmail.com>
2019-11-14 11:42:34 +0100

    some comments in formation_distance class

--- formation_distance.py
+++ formation_distance.py
@@ -1,30 +1,41 @@
 import numpy as np
 from scipy import linalg as la
 
+
+# The symbols are taken from the PhD Thesis "Distributed formation control for autonomous robots" University of Groningen.
+
 class formation_distance:
     def __init__(self, m, l, d, mu, tilde_mu, B, c_shape, c_vel):
-        self.m = m
-        self.l = l
-        self.d = d
-        self.mu = mu
-        self.tilde_mu = tilde_mu
-        self.B = B
+        self.m = m # Dimension {2,3}
+        self.l = l # Order of the Lyapunov function (1,2,...)
+        self.d = d # Set of desired distances (linked to the set of edges)
+        self.mu = mu # Set of mismatches
+        self.tilde_mu = tilde_mu # Set of mismatches
+        self.B = B # Incidence matrix
         self.agents, self.edges = self.B.shape
-        self.S1 = self.make_S1()
-        self.S2 = self.make_S2()
-        self.Av = self.make_Av()
-        self.Aa = self.Av.dot(self.B.T).dot(self.Av)
+        self.S1 = self.make_S1() # Aux matrix
+        self.S2 = self.make_S2() # Aux matrix
+        self.Av = self.make_Av() # Matrix A for eventual velocities
+        self.Aa = self.Av.dot(self.B.T).dot(self.Av) # Matrix A for eventual acceleration
+
+        # Kronecker products
         self.Bb = la.kron(self.B, np.eye(self.m))
         self.S1b = la.kron(self.S1, np.eye(self.m))
         self.S2b = la.kron(self.S2, np.eye(self.m))
         self.Avb = la.kron(self.Av, np.eye(self.m))
         self.Aab = la.kron(self.Aa, np.eye(self.m))
+
+        # Distance error vector
         self.Ed = np.zeros(self.edges)
+
+        # Velocity error vector
         self.Ev = np.zeros(self.edges*self.m)
 
+        # Gain controllers
         self.c_shape = c_shape
         self.c_vel = c_vel
 
+    # Desired acceleration given \ddot p = u
     def u_acc(self, X, V):
         Z = self.Bb.T.dot(X)
         Dz = self.make_Dz(Z)
@@ -35,6 +46,7 @@ class formation_distance:
                 - self.c_vel*V
         return U
 
+    # Desired velocities given \dot p = u
     def u_vel(self, X):
         Z = self.Bb.T.dot(X)
         Dz = self.make_Dz(Z)
@@ -44,6 +56,7 @@ class formation_distance:
                 + self.Avb.dot(Z)
         return U
 
+    # Construct S1 from B
     def make_S1(self):
         S1 = np.zeros_like(self.B)
 
@@ -54,6 +67,7 @@ class formation_distance:
 
         return S1
 
+    # Construct S2 from B
     def make_S2(self):
         S2 = np.zeros_like(-self.B)
 
@@ -63,6 +77,7 @@ class formation_distance:
                     S2[i,j] = 1
         return S2
 
+    # Incidence matrix Bd given the first agent is a leader
     def make_Bd(self):
         Bd = np.copy(self.B)
 
@@ -71,6 +86,7 @@ class formation_distance:
 
         return Bd
 
+    # Diagonal matrix spliting the z elements of Z
     def make_Dz(self, Z):
         Dz = np.zeros((Z.size, self.edges))
     
@@ -82,6 +98,7 @@ class formation_distance:
 
         return Dz
 
+    # Diagonal matrix spliting the z/||z|| elements of Z
     def make_Dzt(self, Z):
         if self.l == 2:
             return np.eye(self.edges)
@@ -89,9 +106,10 @@ class formation_distance:
         Zt = np.zeros(self.edges)
         for i in range(0, self.edges):
             Zt[i] = (la.norm(Z[(i*self.m):(i*self.m+self.m)]))**(self.l-2)
-        
+
         return np.diag(Zt)
 
+    # Construct distance error vector
     def make_E(self, Z):
         E = np.zeros(self.edges)
         for i in range(0, self.edges):
@@ -100,6 +118,7 @@ class formation_distance:
 
         return E
 
+    # Construct A matric for desired velocities
     def make_Av(self):
         A = np.zeros(self.B.shape)
         for i in range(0, self.agents):
